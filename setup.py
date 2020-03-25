a4838073bc2c217c95dcec4975f29309b0d35970
Maksim <maksimpooh@gmail.com>
2020-03-26 00:52:00 +0200

    add button

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def start_handler(message: types.Message):
     btn_app = types.InlineKeyboardButton(
         text='Получить ссылку', url='https://play.google.com/store/apps/details?id=com.smartsvitapp')
     markup.add(btn_app)
-    bot.reply_to(message, "Привет! Я телеграм-бот приложения Smart Svit. Сейчас я могу выполнить только одно действие - поделиться с тобой ссылкой на скачивание приложения. Но скоро я научусь и другим вещам. До встречи :)")
+    bot.reply_to(message, "Привет! Я телеграм-бот приложения Smart Svit. Сейчас я могу выполнить только одно действие - поделиться с тобой ссылкой на скачивание приложения. Но скоро я научусь и другим вещам :)")
     bot.send_message(
         message.chat.id, "Нажми на кнопку и получи приложение для Android", reply_markup=markup)
 
