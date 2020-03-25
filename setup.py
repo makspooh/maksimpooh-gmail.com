3ace2a4a319d761845c0d761defe5e4288393ce4
Maksim <maksimpooh@gmail.com>
2020-03-26 00:49:07 +0200

    add button

--- setup.py
+++ setup.py
@@ -7,15 +7,11 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_handler(message: types.Message):
-    bot.reply_to(message, "Привет! Я телеграм-бот приложения Smart Svit. Сейчас я могу выполнить только одно действие - поделиться с тобой ссылкой на скачивание приложения. Но скоро я научусь и другим вещам. До встречи :)")
-
-
-@bot.message_handler(commands=['url'])
-def url(message):
     markup = types.InlineKeyboardMarkup()
     btn_app = types.InlineKeyboardButton(
         text='Получить ссылку', url='https://play.google.com/store/apps/details?id=com.smartsvitapp')
     markup.add(btn_app)
+    bot.reply_to(message, "Привет! Я телеграм-бот приложения Smart Svit. Сейчас я могу выполнить только одно действие - поделиться с тобой ссылкой на скачивание приложения. Но скоро я научусь и другим вещам. До встречи :)")
     bot.send_message(
         message.chat.id, "Нажми на кнопку и получи ссылку на приложение Android", reply_markup=markup)
 
