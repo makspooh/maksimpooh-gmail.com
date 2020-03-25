cdf33bd9349eb6f50ed2d33ea083d174f46c3207
Maksim <maksimpooh@gmail.com>
2020-03-26 00:42:12 +0200

    add button

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@ from telebot import types
 
 TOKEN = '966690951:AAEQeVK0aHPsHuTek8RQAieynjtVgFjuMY4'
 bot = telebot.TeleBot(TOKEN)
-markup = types.ReplyKeyboardMarkup(row_width=2)
-getAppLink = types.KeyboardButton('Получить ссылку')
-markup.add(getAppLink)
-bot.send_message("Привет! Я телеграм-бот приложения Smart Svit. Сейчас я могу выполнить только одно действие - поделиться с тобой ссылкой на скачивание приложения. Но скоро я научусь и другим вещам. До встречи :)", reply_markup=markup)
 
 
-@bot.message_handler(commands=['help'])
-def help_handler(message: types.Message):
-    bot.reply_to(message, ":)")
+@bot.message_handler(commands=['start'])
+def start_handler(message: types.Message):
+    bot.reply_to(message, "Привет! Я телеграм-бот приложения Smart Svit. Сейчас я могу выполнить только одно действие - поделиться с тобой ссылкой на скачивание приложения. Но скоро я научусь и другим вещам. До встречи :)")
 
 
-@bot.message_handler(content_types=['text'])
-def message_handler(message: types.Message):
-    bot.reply_to(message, 'Извини, я тебя не понимаю :)')
+@bot.message_handler(commands=['url'])
+def url(message):
+    markup = types.InlineKeyboardMarkup()
+    btn_app = types.InlineKeyboardButton(
+        text='Получить ссылку', url='https://play.google.com/store/apps/details?id=com.smartsvitapp')
+    markup.add(btn_app)
+    bot.send_message(
+        message.chat.id, "Нажми на кнопку и получи ссылку на приложение Android", reply_markup=markup)
 
 
 bot.polling()
