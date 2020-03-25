2a6bd82f11f295fb3bf26a003168ca00de95f399
Maksim <maksimpooh@gmail.com>
2020-03-26 00:30:03 +0200

    add button

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 import telebot
-from telebot.types import Message
+from telebot import types
 
 TOKEN = '966690951:AAEQeVK0aHPsHuTek8RQAieynjtVgFjuMY4'
 bot = telebot.TeleBot(TOKEN)
-
-
-@bot.message_handler(commands=['start'])
-def start_handler(message: Message):
-    bot.reply_to(message, "Привет! Я телеграм-бот приложения Smart Svit. Сейчас я могу выполнить только одно действие - поделиться с тобой ссылкой на скачивание приложения. Но скоро я научусь и другим вещам. До встречи :)")
+markup = types.ReplyKeyboardMarkup(row_width=2)
+getAppLink = types.KeyboardButton('Получить ссылку')
+markup.add(getAppLink)
+bot.send_message("Привет! Я телеграм-бот приложения Smart Svit. Сейчас я могу выполнить только одно действие - поделиться с тобой ссылкой на скачивание приложения. Но скоро я научусь и другим вещам. До встречи :)", reply_markup=markup)
 
 
 @bot.message_handler(commands=['help'])
-def help_handler(message: Message):
-    bot.reply_to(message, "Help clicked")
+def help_handler(message: types.Message):
+    bot.reply_to(message, ":)")
 
 
 @bot.message_handler(content_types=['text'])
-def message_handler(message: Message):
-    bot.reply_to(message, 'hello there')
+def message_handler(message: types.Message):
+    bot.reply_to(message, 'Извини, я тебя не понимаю :)')
 
 
 bot.polling()
